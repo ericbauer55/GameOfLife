80d00374127e74a4bfce4d9509564e4b79052abc
vulcant <eric.bauer@gmail.com>
2019-09-06 13:41:45 -0400

    reduces the complexity of the map to debug the apply_rules method

--- game_of_life.py
+++ game_of_life.py
@@ -1,4 +1,4 @@
-from typing import List, NamedTuple, TypeVar, Generic
+from typing import List, NamedTuple
 from enum import Enum
 import random
 
@@ -26,6 +26,7 @@ class Game:
         self._liveliness: float = liveliness
         # generate random setup
         self._current_gen: Generation = Game._randomize_generation(rows, columns, liveliness)
+        self._next_gen: Generation = self._current_gen
 
     @staticmethod
     def _randomize_generation(rows, cols, liveliness) -> Generation:
@@ -37,56 +38,55 @@ class Game:
                     generation[r][c] = Cell.ALIVE
         return generation
 
-    def _apply_rules(self, generation: Generation) -> Generation:
+    def _apply_rules(self) -> None:
         """Applies the rules grid cell by grid cell to update the next generation"""
         # generations are double buffered so that the application of rules doesn't overwrite the current generation
-        next_gen: Generation = generation
         for r in range(self._rows):
             for c in range(self._cols):
                 alive_cells: int = 0
                 # for each cell, count the number of alive neighbor cells (8 if statements), don't go out of bounds
                 # check North West
-                if r - 1 >= 0 and c - 1 >= 0 and generation[r][c] == Cell.ALIVE:
+                if r - 1 >= 0 and c - 1 >= 0 and self._current_gen[r][c] == Cell.ALIVE:
                     alive_cells += 1
 
                 # check North
-                if r - 1 >= 0 and generation[r][c] == Cell.ALIVE:
+                if r - 1 >= 0 and self._current_gen[r][c] == Cell.ALIVE:
                     alive_cells += 1
 
                 # check North East
-                if r - 1 >= 0 and c + 1 < self._cols and generation[r][c] == Cell.ALIVE:
+                if r - 1 >= 0 and c + 1 < self._cols and self._current_gen[r][c] == Cell.ALIVE:
                     alive_cells += 1
 
                 # check East
-                if c + 1 < self._cols and generation[r][c] == Cell.ALIVE:
+                if c + 1 < self._cols and self._current_gen[r][c] == Cell.ALIVE:
                     alive_cells += 1
 
                 # check South East
-                if r + 1 < self._rows and c + 1 < self._cols and generation[r][c] == Cell.ALIVE:
+                if r + 1 < self._rows and c + 1 < self._cols and self._current_gen[r][c] == Cell.ALIVE:
                     alive_cells += 1
 
                 # check South
-                if r + 1 < self._rows and generation[r][c] == Cell.ALIVE:
+                if r + 1 < self._rows and self._current_gen[r][c] == Cell.ALIVE:
                     alive_cells += 1
 
                 # check South West
-                if r + 1 < self._rows and c - 1 >= 0 and generation[r][c] == Cell.ALIVE:
+                if r + 1 < self._rows and c - 1 >= 0 and self._current_gen[r][c] == Cell.ALIVE:
                     alive_cells += 1
 
                 # check West
-                if c - 1 >= 0 and generation[r][c] == Cell.ALIVE:
+                if c - 1 >= 0 and self._current_gen[r][c] == Cell.ALIVE:
                     alive_cells += 1
 
                 # based on the number of alive cells, apply different rules:
-                if alive_cells < 2 and generation[r][c] == Cell.ALIVE:
-                    next_gen[r][c] = Cell.DEAD  # under population
-                elif (alive_cells == 2 or alive_cells == 3) and generation[r][c] == Cell.ALIVE:
-                    next_gen[r][c] = Cell.ALIVE  # okay population
-                elif alive_cells > 3 and generation[r][c] == Cell.ALIVE:
-                    next_gen[r][c] = Cell.DEAD  # over population
-                elif alive_cells == 3 and generation[r][c] == Cell.DEAD:
-                    next_gen[r][c] = Cell.ALIVE  # reproduction
-        return next_gen
+                if alive_cells < 2 and self._current_gen[r][c] == Cell.ALIVE:
+                    self._next_gen[r][c] = Cell.DEAD  # under population
+                elif (alive_cells == 2 or alive_cells == 3) and self._current_gen[r][c] == Cell.ALIVE:
+                    self._next_gen[r][c] = Cell.ALIVE  # okay population
+                elif alive_cells > 3 and self._current_gen[r][c] == Cell.ALIVE:
+                    self._next_gen[r][c] = Cell.DEAD  # over population
+                elif alive_cells == 3 and self._current_gen[r][c] == Cell.DEAD:
+                    self._next_gen[r][c] = Cell.ALIVE  # reproduction
+        self._current_gen = self._next_gen  # update the generation
 
     def __str__(self) -> str:
         """Print the current generation"""
@@ -102,10 +102,10 @@ class Game:
             if verbose:
                 print("Season #{}".format(k))
                 print(self)
-            self._current_gen = self._apply_rules(self._current_gen)
-
+            self._apply_rules()
 
 
 if __name__ == '__main__':
-    game: Game = Game(6, 18, 0.20)
-    game.simulate(10)
+    random.seed(10)
+    game: Game = Game(3, 3, 0.50)
+    game.simulate(3)

4b9e161d16351e0980755477ef2c88211a813dbd
vulcant <eric.bauer@gmail.com>
2019-09-06 12:44:20 -0400

    Created a method to simulate the rules of the GoL by looping for a number of seasons to apply the rules

--- game_of_life.py
+++ game_of_life.py
@@ -24,18 +24,23 @@ class Game:
         self._rows: int = rows
         self._cols: int = columns
         self._liveliness: float = liveliness
-
         # generate random setup
         self._current_gen: Generation = Game._randomize_generation(rows, columns, liveliness)
 
     @staticmethod
     def _randomize_generation(rows, cols, liveliness) -> Generation:
         """Creates a randomized generation"""
-        generation: Generation = [[Cell.DEAD for c in range(cols)] for r in range(rows)]
+        generation: Generation = [[Cell.DEAD for _ in range(cols)] for _ in range(rows)]
         for r in range(rows):
             for c in range(cols):
                 if random.uniform(0, 1.0) < liveliness:
                     generation[r][c] = Cell.ALIVE
+        return generation
+
+    @staticmethod
+    def _apply_rules(generation: Generation) -> Generation:
+        """Applies the rules grid cell by grid cell to update the next generation"""
+        # generations are double buffered so that the application of rules doesn't overwrite the current generation
         return generation
 
     def __str__(self) -> str:
@@ -46,7 +51,15 @@ class Game:
             output += "\n"
         return output
 
+    def simulate(self, n_seasons: int, verbose: bool = True):
+        """Simulates the Rules of the Game of Life for n number of seasons"""
+        for k in range(n_seasons):
+            self._current_gen = self._apply_rules(self._current_gen)
+            if verbose:
+                print("Season #{}".format(k))
+                print(self)
+
 
 if __name__ == '__main__':
     game: Game = Game(6, 18, 0.20)
-    print(game)
+    game.simulate(10)
